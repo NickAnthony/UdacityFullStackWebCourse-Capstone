698f4853f077e7db40e87d31362a9b57bcb84b65
Nick Anthony <nickanthony45@gmail.com>
2021-03-03 07:39:20 -0500

    Add scaffolding for new database tables

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, Date, create_engine
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 import json, os
 
@@ -18,7 +19,7 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
-
+    migrate = Migrate(app, db)
 
 '''
 Person
@@ -40,3 +41,60 @@ class Person(db.Model):
       'id': self.id,
       'name': self.name,
       'catchphrase': self.catchphrase}
+
+'''
+Actor
+Represents an actor with attributes name, age, and gender.
+'''
+# class Actor(db.Model):
+#   __tablename__ = 'actors'
+#
+#   id = Column(Integer, primary_key=True)
+#   name = db.Column(String, nullable=False)
+#   age = db.Column(Integer, nullable=False)
+#   gender = db.Column(String, nullable=False)
+#   # @TODO: Establish many to many relationship with movies
+#   # movies = db.relationship('Movie', backref='actor', lazy=True, cascade="all, delete")
+#
+#   def __init__(self, name, age, gender):
+#     self.name = name
+#     self.age = age
+#     self.gender = gender
+#
+#   def format(self):
+#     return {
+#       'id': self.id,
+#       'name': self.name,
+#       'age': self.age,
+#       'gender': self.gender}
+#
+#   def __repr__(self):
+#     return f'<Actor {self.id} {self.name}>'
+
+'''
+Movie
+Represents a movie with attributes title and release date.
+'''
+# class Movie(db.Model):
+#   __tablename__ = 'movies'
+#
+#   id = Column(Integer, primary_key=True)
+#   title = Column(String, nullable=False)
+#   # A datetime.date() object
+#   release_date = Column(Date, nullable=False)
+#   # @TODO: Establish many to many relationship with actors
+#   # actor_id = db.Column(db.Integer, db.ForeignKey('actors.id'),
+#   #                      nullable=False, default=1)
+#
+#   def __init__(self, title, release_date):
+#     self.title = title
+#     self.release_date = release_date
+#
+#   def format(self):
+#     return {
+#       'id': self.id,
+#       'title': self.title,
+#       'release_date': "{:%m/%d/%Y}".format(self.release_date)}
+#
+#   def __repr__(self):
+#     return f'<Movie {self.id} {self.title}>'
