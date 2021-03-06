eed6f655944487ad66046a1c03ac056f9c1163ab
Nick Anthony <nickanthony45@gmail.com>
2021-03-06 13:01:40 -0500

    Add movie GET/DELETE/POST/PATCH endpoints and clean up various methods/docs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import datetime, os
 from flask import Flask
 from flask_cors import CORS
 from models import setup_db
@@ -35,6 +35,9 @@ def create_app(test_config=None):
     def format_actors(actors):
         return [actor.format() for actor in actors]
 
+    def format_movies(movies):
+        return [movie.format() for movie in movies]
+
     #----------------------------------------------------------------------------#
     # Actor endpoints/routes.
     #----------------------------------------------------------------------------#
@@ -70,7 +73,7 @@ def create_app(test_config=None):
     '''
     @app.route('/actors/<int:actor_id>', methods=['DELETE'])
     def delete_actor(actor_id):
-        actor_to_delete = Actory.query.get(actor_id)
+        actor_to_delete = Actor.query.get(actor_id)
         if not actor_to_delete:
             abort(404)
         try:
@@ -97,6 +100,7 @@ def create_app(test_config=None):
           - Status code 200 and json {"success": True, "actors": [actor]} where
               actors is an array containing only the newly created actor
               or appropriate status code indicating reason for failure
+    @TODO: Implement ability to associate an actor with a movie.
     '''
     @app.route('/actors', methods=['POST'])
     def add_new_actor(payload):
@@ -127,7 +131,7 @@ def create_app(test_config=None):
             abort(422)
 
     '''
-    PATCH /drinks/<id>
+    PATCH /actors/<id>
         - Updates an existing actor.  It will throw a 404 if <id> is not found.
         - Permissions required:
             - 'patch:actors'
@@ -136,14 +140,15 @@ def create_app(test_config=None):
             - [Optional] 'age': An Integer that is the age of the actor.
             - [Optional] 'gender': A string that is the gender of the actor.
             - The actor information will not change if none of the request
-                arguments are supplied.  However, a 200 will be returned.
+                arguments are supplied.  However, a 200 will still be returned.
         - Returns:
           - Status code 200 and json {"success": True, "actors": [actor]} where
               actors is an array containing only the updated actor
               or appropriate status code indicating reason for failure.
+    @TODO: Implement ability to update the movies an actor is in.
     '''
     @app.route('/actors/<int:actor_id>', methods=['PATCH'])
-    def modify_exiting_drink(actor_id):
+    def modify_exiting_actor(actor_id):
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
         if not actor:
             abort(404)
@@ -168,6 +173,157 @@ def create_app(test_config=None):
             })
         except Exception as e:
             abort(422)
+
+    #----------------------------------------------------------------------------#
+    # Movie endpoints/routes.
+    #----------------------------------------------------------------------------#
+    '''
+    GET /movies
+        - A Public Endpoint that fetches a list of all movies. If there are
+            no movies, it will return an empty list.
+        - Permissions required: None
+        - Request Arguments: None
+        - Returns:
+          - Status code 200 and json {"success": True, "movies": movies}
+            where movies is a list of all movies.
+    '''
+    @app.route('/movies', methods=['GET'])
+    def get_movies():
+        movies = Movie.query.all()
+        return jsonify({
+                'success': True,
+                'movies': format_movies(movies)
+            })
+
+    '''
+    DELETE /movies
+        - A Public Endpoint that deletes an existing movie from the database.
+          Returns a 404 if the movie <id> is not found.
+        - Permissions required:
+          - 'delete:movies'
+        - Request Arguments: None
+        - Returns:
+          - Status code 200 and json {"success": True, "delete": id} where id
+              is the id of the deleted record or appropriate status code
+              indicating reason for failure.
+    '''
+    @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+    def delete_movie(movie_id):
+        movie_to_delete = Movie.query.get(movie_id)
+        if not movie_to_delete:
+            abort(404)
+        try:
+            movie_to_delete_id = movie_to_delete.id
+            movie_to_delete.delete()
+            return jsonify({
+                'success': True,
+                'delete': movie_to_delete_id
+            })
+        except Exception as e:
+            abort(422)
+
+    '''
+    POST /movies
+        - Creates a new movies and stores it in the database.  It will throw
+            a 400 if the incorrect parameters are passed.
+        - Permissions required:
+            - 'post:movies'
+        - Request Arguments:
+            - 'title': A string that is the full title of the movie.
+            - 'release_date': A string of the release date of the movie, in the
+               format "YYYY-MM-DD"
+        - Returns:
+          - Status code 200 and json {"success": True, "movies": [movie]} where
+              movies is an array containing only the newly created movie
+              or appropriate status code indicating reason for failure
+    @TODO: Implement ability to associate actors with this movie.
+    '''
+    @app.route('/movies', methods=['POST'])
+    def add_new_movie(payload):
+        if not request.get_json():
+            abort(400)
+        title = request.get_json().get('title', None)
+        release_date_str = request.get_json().get('release_date', None)
+
+        # Verify that the appropriate parameters were passed.
+        if (not title or not release_date_str):
+            abort(400)
+
+        # Verify that the date is in a valid format and a valid date.
+        try:
+            release_date = datetime.datetime.strptime(release_date_str,
+                                                      "%Y-%m-%d").date()
+        except ValueError as e:
+            abort(400)
+
+        try:
+            new_movie = Movie(
+                title=title,
+                release_date=release_date
+            )
+            new_movie.insert()
+            # We can get the id of the new_movie because it has been flushed.
+            return jsonify({
+                'success': True,
+                'id': new_movie.id,
+                'movies': format_movies([new_movie])
+            })
+        except Exception as e:
+            abort(422)
+
+    '''
+    PATCH /movies/<id>
+        - Updates an existing movie.  It will throw a 404 if <id> is not found.
+        - Permissions required:
+            - 'patch:movies'
+        - Request Arguments:
+            - [Optional] 'title': A string that is the full title of the movie.
+            - [Optional] 'release_date': A string of the release date of the movie, in the
+               format "YYYY-MM-DD"
+            - The movie information will not change if none of the request
+                arguments are supplied.  However, a 200 will still be returned.
+        - Returns:
+          - Status code 200 and json {"success": True, "movies": [movie]} where
+              movies is an array containing only the updated movie
+              or appropriate status code indicating reason for failure.
+    @TODO: Implement ability to modify the actors in this movie.
+    '''
+    @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+    def modify_exiting_movie(movie_id):
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        if not movie:
+            abort(404)
+
+        req_json = equest.get_json()
+        title = req_json.get('title', None)
+        release_date_str = req_json.get(
+            'release_date',
+            "{:%Y-%m-%d}".format(self.release_date)
+        )
+
+        # Verify that the appropriate parameters were passed.
+        if (not title or not release_date_str):
+            abort(400)
+
+        # Verify that the date is in a valid format and a valid date.
+        try:
+            release_date = datetime.datetime.strptime(release_date_str,
+                                                      "%Y-%m-%d").date()
+        except ValueError as e:
+            abort(400)
+
+        try:
+            movie.title = title
+            movie.release_date = release_date
+            movie.update()
+            return jsonify({
+                'success': True,
+                'id': movie.id,
+                'movies': format_movies([movie])
+            })
+        except Exception as e:
+            abort(422)
+
 
     return app
 
