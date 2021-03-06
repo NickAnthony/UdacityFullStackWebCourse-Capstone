ff4ff09f6746cddb89104cd5e877ddaef2ee4db7
Nick Anthony <nickanthony45@gmail.com>
2021-03-06 08:27:14 -0500

    Initial migration and fix issue with migration version not being created

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,9 @@ from flask import current_app
 
 from alembic import context
 
+from models import *
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
