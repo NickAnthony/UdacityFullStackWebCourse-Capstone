f3d19f0beb00b00d2ebcb91ec52d2f8465acfdb1
Nick Anthony <nickanthony45@gmail.com>
2021-04-04 10:22:43 -0400

    Change actors and movie display to be images and portraits

--- frontend/src/components/ActorColumn.js
+++ frontend/src/components/ActorColumn.js
@@ -5,11 +5,16 @@ class ActorColumn extends React.Component {
     return (
       <div className="Actor-column">
         <h2 className="Actor-column-header">Actors</h2>
-        <ul>
-          { this.props.actors.map((actor, index) => {
-              return <li key={index}>{actor.name}</li>
-           })}
-        </ul>
+        {
+          this.props.actors.map((actor, index) =>
+            <div className="Actor-row">
+              <img src={actor.portait_url} alt={actor.name + " Portait"} width="50"></img>
+              <div className="Actor-row-name">
+                <div>{actor.name}</div>
+              </div>
+            </div>
+          )
+        }
       </div>
     );
   }
--- frontend/src/components/MovieColumn.js
+++ frontend/src/components/MovieColumn.js
@@ -3,13 +3,18 @@ import React from 'react';
 class MovieColumn extends React.Component {
   render() {
     return (
-      <div className="Movie-column">
+      <div className="Movie-column-wrapper">
         <h2 className="Movie-column-header">Movies</h2>
-        <ul>
-          { this.props.movies.map((movie, index) => {
-              return <li key={index}>{movie.title}</li>
-           })}
-        </ul>
+        <div className="Movie-column">
+          { this.props.movies.map((movie, index) =>
+            <div className="Movie-thumbnail">
+              <img src={movie.movie_photo} alt={movie.title + " Image"} height="250"></img>
+              <div className="Movie-date">
+                <div>{movie.release_date}</div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
