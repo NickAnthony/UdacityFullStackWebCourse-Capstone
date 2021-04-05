f493d2d2a0ca419a709d7412e64a38a08d0b4299
Nick Anthony <nickanthony45@gmail.com>
2021-04-05 07:20:21 -0400

    Add a formatted login button

--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class AppHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    // This binding is necessary to make `this` work in the callback
+    this.loginWithAuth0 = this.loginWithAuth0.bind(this);
+  }
+  loginWithAuth0(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
   render() {
     return (
       <div className="App-header">
         <h2>CASTING AGENCY</h2>
+        <div className="Menu">
+          <button onClick={this.loginWithAuth0} className="Menu-button">
+            Login
+          </button>
+        </div>
       </div>
     );
   }
