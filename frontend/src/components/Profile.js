4b5d8eebb6209dc5d390188e50b11798a9e02d9d
Nick Anthony <nickanthony45@gmail.com>
2021-05-02 18:09:28 -0400

    Add AppLoader to the profile page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import AppLoader from './AppLoader';
 
 const no_portait_placeholder = "https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading, error } = useAuth0();
 
-  console.log("User:", user)
-  console.log("isAuthenticated:", isAuthenticated)
-  console.log("isLoading:", isLoading)
-  console.log("error:", error)
-
   if (isLoading) {
-    return <div><h2>Loading ...</h2></div>;
+    return <AppLoader />;
   }
   if (!isAuthenticated) {
     return (
