8c0a12042f192eb75c549fedea69cd0fb32e1927
Nick Anthony <nickanthony45@gmail.com>
2021-05-04 07:28:45 -0400

    npx eslint index.js fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,22 +4,22 @@ import './styles/index.css';
 import './styles/DatePicker.css';
 import './styles/Calendar.css';
 import App from './App';
-import { Auth0Provider } from "@auth0/auth0-react";
+import {Auth0Provider} from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="fsnd-app-nickanthony.us.auth0.com"
-      clientId="ExP2mxHo4wAMYB0MGc9nmWHxSHcfO1eu"
-      redirectUri={window.location.origin + "/profile"}
-      audience="casting-agency"
-      scope="read:current_user update:current_user_metadata"
-    >
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <Auth0Provider
+        domain="fsnd-app-nickanthony.us.auth0.com"
+        clientId="ExP2mxHo4wAMYB0MGc9nmWHxSHcfO1eu"
+        redirectUri={window.location.origin + '/profile'}
+        audience="casting-agency"
+        scope="read:current_user update:current_user_metadata"
+      >
+        <App />
+      </Auth0Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
