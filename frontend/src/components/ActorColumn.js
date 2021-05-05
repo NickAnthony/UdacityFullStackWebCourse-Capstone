04279fa894299189c2fbef087cb7e2d354df9d3a
Nick Anthony <nickanthony45@gmail.com>
2021-05-05 06:53:37 -0400

    npx eslint ActorColumn.js

--- frontend/src/components/ActorColumn.js
+++ frontend/src/components/ActorColumn.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import {DOMAIN, noPortraitPlaceholder} from '../Constants.js';
 import ActorRow from './ActorRow';
-import { DOMAIN, noPortraitPlaceholder } from '../Constants.js';
 import AppLoader from './AppLoader';
 
+/**
+ * Represents a column of actors and actresses.
+ * Fetches the /actors from the database then constructs an array of ActorRows.
+ * @component
+ */
 class ActorColumn extends React.Component {
+  /**
+   * Constructs ActorColumn and sets initial state.
+   * @constructor
+   * @param {object} props -  the default props object
+   */
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-       actors: []
-     }
+      actors: [],
+    };
   }
+  /**
+   * Runs when component mounts. Fetches the list of actors from the database.
+   */
   componentDidMount() {
-    fetch(DOMAIN + "/actors")
-        .then(response => response.json())
+    fetch(DOMAIN + '/actors')
+        .then((response) => response.json())
         .then((result) => {
-          this.setState({ actors: result.actors });
+          this.setState({actors: result.actors});
         });
   }
+  /**
+   * Returns the constructed components and html.
+   * If the RPC call has no yet returned, shows a loading icon.
+   * @return {object} the list of ActorRows for all actors.
+   */
   render() {
     if (this.state.actors === undefined || this.state.actors.length === 0) {
       return (
@@ -29,16 +47,16 @@ class ActorColumn extends React.Component {
         <h3 className="Column-header">Available</h3>
         {
           this.state.actors.map((actor, index) => {
-            var image_src = noPortraitPlaceholder
+            let imageSrc = noPortraitPlaceholder;
             if (actor.portrait_url !== undefined) {
-              image_src = actor.portrait_url;
+              imageSrc = actor.portrait_url;
             }
             return <ActorRow
-                      actor_id={actor.id}
-                      image_src={image_src}
-                      key={index}
-                      index={index}
-                      name={actor.name}/>;
+              actor_id={actor.id}
+              image_src={imageSrc}
+              key={index}
+              index={index}
+              name={actor.name}/>;
           })
         }
         <h3 className="Column-header">Unavabilable</h3>
