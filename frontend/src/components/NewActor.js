018fb820c8c1eaf914ae2f2e6df9bae9474cd7aa
Nick Anthony <nickanthony45@gmail.com>
2021-05-24 07:00:43 -0400

    npx eslint NewActor.js --fix

--- frontend/src/components/NewActor.js
+++ frontend/src/components/NewActor.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Redirect } from 'react-router-dom';
-import { DOMAIN, noPortraitPlaceholder } from '../Constants.js'
+import React, {useState} from 'react';
+import {useAuth0} from '@auth0/auth0-react';
+import {Redirect} from 'react-router-dom';
+import {DOMAIN, noPortraitPlaceholder} from '../Constants.js';
 
 /**
  * Represent the new actor route at "/new-actor".  This page lets you add an
@@ -9,15 +9,15 @@ import { DOMAIN, noPortraitPlaceholder } from '../Constants.js'
  * @return {Component} NewActor component to create new actors.
  */
 function NewActor() {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const {isAuthenticated, getAccessTokenSilently} = useAuth0();
 
   // Set up state
-  const [portrait_url, setPortraitUrl] = useState("");
-  const [portrait_img_src, setPortraitImgSrc] = useState(noPortraitPlaceholder);
-  const [name, setName] = useState("");
+  const [portraitUrl, setPortraitUrl] = useState('');
+  const [portraitImgSrc, setPortraitImgSrc] = useState(noPortraitPlaceholder);
+  const [name, setName] = useState('');
   const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("");
-  const [redirect, setRedirect] = useState("");
+  const [gender, setGender] = useState('');
+  const [redirect, setRedirect] = useState('');
 
   const handleChange = (event) => {
     setPortraitUrl(event.target.value);
@@ -26,22 +26,28 @@ function NewActor() {
     } else {
       setPortraitImgSrc(noPortraitPlaceholder);
     }
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     postNewActor();
-  }
+  };
 
+  /**
+   * Takes the current input and posts the new actor to the database.
+   * Requires user to have logged in and have the correct ACLs.  New
+   * actors/actresses can only be posted by Executive Producers or Casting
+   * Agents.
+   */
   const postNewActor = async () => {
     try {
       // TO-DO Make this occur on page load, not on submit
       const accessToken = await getAccessTokenSilently({
         audience: `casting-agency`,
-        scope: "read:current_user",
+        scope: 'read:current_user',
       });
       // TO-DO Make this Async
-      fetch(DOMAIN + "/actors", {
+      fetch(DOMAIN + '/actors', {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${accessToken}`,
@@ -51,16 +57,15 @@ function NewActor() {
           name: name,
           age: age,
           gender: gender,
-          portrait_url: portrait_url,
-        })
+          portrait_url: portraitUrl,
+        }),
       })
-      .then(response => response.json())
-      .then((result) => {
-        if (result.success) {
-          setRedirect(`/actors/${result.id}`)
-        }
-      });
-
+          .then((response) => response.json())
+          .then((result) => {
+            if (result.success) {
+              setRedirect(`/actors/${result.id}`);
+            }
+          });
     } catch (e) {
       console.log(e.message);
     }
@@ -70,57 +75,61 @@ function NewActor() {
     return <Redirect to={redirect} />;
   }
   if (!isAuthenticated) {
-    return(
+    return (
       <div className="New-item-wrapper">
         <h1>Please log in to add actors</h1>
       </div>
-    )
+    );
   }
 
   return (
-      <div className="New-item-wrapper">
-        <form className="New-item-form-wrapper" onSubmit={handleSubmit}>
-          <h1>New Actor</h1>
-          <p className="Form-label">Name</p>
-          <input
-            className="Form-input"
-            type="text"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-          <p className="Form-label">Age</p>
-          <input
-            className="Form-input"
-            type="number"
-            value={age}
-            onChange={e => setAge(e.target.value)}
-          />
-          <p className="Form-label">Gender</p>
-          <input
-            className="Form-input"
-            type="text"
-            value={gender}
-            onChange={e => setGender(e.target.value)}
-          />
-          <p className="Form-label">Portait Photo</p>
-          <input
-            className="Form-input"
-            type="url"
-            value={portrait_url}
-            onChange={handleChange}/>
+    <div className="New-item-wrapper">
+      <form className="New-item-form-wrapper" onSubmit={handleSubmit}>
+        <h1>New Actor</h1>
+        <p className="Form-label">Name</p>
+        <input
+          className="Form-input"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <p className="Form-label">Age</p>
+        <input
+          className="Form-input"
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <p className="Form-label">Gender</p>
+        <input
+          className="Form-input"
+          type="text"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        <p className="Form-label">Portait Photo</p>
+        <input
+          className="Form-input"
+          type="url"
+          value={portraitUrl}
+          onChange={handleChange}/>
           &nbsp;
-          <img
-            src={portrait_img_src}
-            alt={`${name} Portait`}
-            width="100"/>
-          {
-            !(portrait_url) &&
-            <p className="Form-label">Image will load with url</p> }
-          <div className="Submit-button-wrapper">
-            <input type="submit" value="Submit" className="Button Submit-button" />
-          </div>
-        </form>
-      </div>
+        <img
+          src={portraitImgSrc}
+          alt={`${name} Portait`}
+          width="100"/>
+        {
+          !(portraitUrl) &&
+            <p className="Form-label">Image will load with url</p>
+        }
+        <div className="Submit-button-wrapper">
+          <input
+            type="submit"
+            value="Submit"
+            className="Button Submit-button" />
+        </div>
+      </form>
+    </div>
   );
 }
 
