3bc36362921315ddc16d2919392e7d9a55e44bc2
Nick Anthony <nickanthony45@gmail.com>
2021-05-24 06:47:36 -0400

    npx eslint Thumbnail.js NonLinkThumbnail.js --fix

--- frontend/src/components/NonLinkThumbnail.js
+++ frontend/src/components/NonLinkThumbnail.js
@@ -1,25 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const NonLinkThumbnail = props => {
+/**
+ * Represents a non-linked Thumbnail.  This means it won't redirect on click
+ * so the on click functionality can be overriden.
+ * Used as the select-deselect thumbnail in the associate components.
+ * If props.selected == true, then it highlights as green.
+ * @param {object} props - the props object.  Requires the following pieces:
+ *     selected - true == highlight as green. false == no highlight.
+ *     index - the index of this thumbnail in the row/column
+ *     imageSrc - the source of the image
+ *     title - the text to be displayed below the image
+ * @return {Component} NonLinkThumbnail component to represent a thumbnail.
+ */
+const NonLinkThumbnail = (props) => {
   if (props.selected) {
     return (
-        <div className="Thumbnail-selected" key={props.index}>
-          <img src={props.imageSrc} alt={props.title + " Image"} height="250"></img>
-          <div className="Thumbnail-text">
-            <div>{props.title}</div>
-          </div>
+      <div className="Thumbnail-selected" key={props.index}>
+        <img
+          src={props.imageSrc}
+          alt={props.title + ' Image'}
+          height="250">
+        </img>
+        <div className="Thumbnail-text">
+          <div>{props.title}</div>
         </div>
+      </div>
     );
   } else {
     return (
-        <div className="Thumbnail" key={props.index}>
-          <img src={props.imageSrc} alt={props.title + " Image"} height="250"></img>
-          <div className="Thumbnail-text">
-            <div>{props.title}</div>
-          </div>
+      <div className="Thumbnail" key={props.index}>
+        <img
+          src={props.imageSrc}
+          alt={props.title + ' Image'}
+          height="250">
+        </img>
+        <div className="Thumbnail-text">
+          <div>{props.title}</div>
         </div>
+      </div>
     );
   }
-}
+};
+
+NonLinkThumbnail.propTypes = {
+  selected: PropTypes.bool,
+  index: PropTypes.number,
+  imageSrc: PropTypes.string,
+  title: PropTypes.string,
+};
 
 export default NonLinkThumbnail;
--- frontend/src/components/Thumbnail.js
+++ frontend/src/components/Thumbnail.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Thumbnail = props => {
-  const profile_link = `/${props.type}/${props.id}`;
+/**
+ * Represents a linked Thumbnail.  On click, it will redirect to the given
+ * resource type and it's id: `/type/id`.
+ * @param {object} props - the props object.  Requires the following pieces:
+ *     type - the resource type, either "actor" or "movie"
+ *     id - the id of the resource
+ *     index - the index of this thumbnail in the row/column
+ *     imageSrc - the source of the image
+ *     title - the text to be displayed below the image
+ * @return {Component} NonLinkThumbnail component to represent a thumbnail.
+ */
+const Thumbnail = (props) => {
+  const profileLink = `/${props.type}/${props.id}`;
   return (
-    <Link to={profile_link} style={{ textDecoration: 'none' }}>
+    <Link to={profileLink} style={{textDecoration: 'none'}}>
       <div className="Thumbnail" key={props.index}>
-        <img src={props.imageSrc} alt={props.title + " Image"} height="250"></img>
+        <img
+          src={props.imageSrc}
+          alt={props.title + ' Image'}
+          height="250">
+        </img>
         <div className="Thumbnail-text">
           <div>{props.title}</div>
         </div>
       </div>
     </Link>
   );
-}
+};
+
+Thumbnail.propTypes = {
+  type: PropTypes.string,
+  id: PropTypes.number,
+  index: PropTypes.number,
+  imageSrc: PropTypes.string,
+  title: PropTypes.string,
+};
 
 export default Thumbnail;
