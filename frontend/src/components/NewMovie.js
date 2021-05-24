db06f6912e08c113ef82e17da1e0aca23385951f
Nick Anthony <nickanthony45@gmail.com>
2021-05-24 06:37:16 -0400

    npx eslint NewMovie.js --fix

--- frontend/src/components/NewMovie.js
+++ frontend/src/components/NewMovie.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React, {useState} from 'react';
+import {useAuth0} from '@auth0/auth0-react';
 import DatePicker from 'react-date-picker';
-import { Redirect } from 'react-router-dom';
-import { DOMAIN, noMoviePlaceholder } from '../Constants.js'
+import {Redirect} from 'react-router-dom';
+import {DOMAIN, noMoviePlaceholder} from '../Constants.js';
 import Moment from 'moment';
 
 /**
@@ -11,15 +11,15 @@ import Moment from 'moment';
   * @return {Component} NewMovie component to create new movies.
  */
 function NewMovie() {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const {isAuthenticated, getAccessTokenSilently} = useAuth0();
   Moment.locale('en');
 
   // Set up state
-  const [movie_photo_url, setMoviePhotoUrl] = useState(null);
-  const [photo_img_src, setPhotoImgSrc] = useState(noMoviePlaceholder);
-  const [title, setTitle] = useState("");
-  const [release_date, setReleaseDate] = useState(new Date());
-  const [redirect, setRedirect] = useState("");
+  const [moviePhotoUrl, setMoviePhotoUrl] = useState(null);
+  const [photoImgSrc, setPhotoImgSrc] = useState(noMoviePlaceholder);
+  const [title, setTitle] = useState('');
+  const [releaseDate, setReleaseDate] = useState(new Date());
+  const [redirect, setRedirect] = useState('');
 
   const handleChange = (event) => {
     setMoviePhotoUrl(event.target.value);
@@ -28,28 +28,35 @@ function NewMovie() {
     } else {
       setPhotoImgSrc(noMoviePlaceholder);
     }
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     postNewMovie();
-  }
-
+  };
+  /**
+   * Takes the current input and posts the movie to the database.
+   * Requires user to have logged in and have the correct ACLs.  New movies can
+   * only be posted by Executive Producers.
+   */
   const postNewMovie = async () => {
     try {
-      // TO-DO Make this occur on page load, not on submit
+      // TODO: Make this occur on page load, not on submit
       const accessToken = await getAccessTokenSilently({
         audience: `casting-agency`,
-        scope: "read:current_user",
+        scope: 'read:current_user',
       });
-      const formatted_release_date = Moment(release_date.toString()).format("YYYY-MM-DD");
+      // eslint-disable-next-line new-cap
+      const formattedReleaseDate = Moment(
+          releaseDate.toString(),
+      ).format('YYYY-MM-DD');
       console.log(JSON.stringify({
         title: title,
-        release_date: formatted_release_date,
-        movie_photo: movie_photo_url,
-      }))
+        releaseDate: formattedReleaseDate,
+        movie_photo: moviePhotoUrl,
+      }));
       // TO-DO Make this Async
-      fetch(DOMAIN + "/movies", {
+      fetch(DOMAIN + '/movies', {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${accessToken}`,
@@ -57,18 +64,17 @@ function NewMovie() {
         },
         body: JSON.stringify({
           title: title,
-          release_date: formatted_release_date,
-          movie_photo: movie_photo_url,
-        })
+          releaseDate: formattedReleaseDate,
+          movie_photo: moviePhotoUrl,
+        }),
       })
-      .then(response => response.json())
-      .then((result) => {
-        console.log(result);
-        if (result.success) {
-          setRedirect(`/movies/${result.id}`)
-        }
-      });
-
+          .then((response) => response.json())
+          .then((result) => {
+            console.log(result);
+            if (result.success) {
+              setRedirect(`/movies/${result.id}`);
+            }
+          });
     } catch (e) {
       console.log(e.message);
     }
@@ -78,51 +84,57 @@ function NewMovie() {
     return <Redirect to={redirect} />;
   }
   if (!isAuthenticated) {
-    return(
+    return (
       <div className="New-item-wrapper">
         <form className="New-item-form-wrapper">
           <h1>New Movie</h1>
-          <h2>Only Executive Producers can add movies.  Please log in to continue.</h2>
+          <h2>Only Executive Producers can add movies.  Please log in to
+              continue.
+          </h2>
         </form>
       </div>
-    )
+    );
   }
 
   return (
-      <div className="New-item-wrapper">
-        <form className="New-item-form-wrapper" onSubmit={handleSubmit}>
-          <h1>New Movie</h1>
-          <p className="Form-label">Title</p>
-          <input
-            className="Form-input"
-            type="text"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-          <p className="Form-label">Release date</p>
-          <DatePicker
-              onChange={setReleaseDate}
-              value={release_date}
-            />
-          <p className="Form-label">Portait Photo</p>
-          <input
-            className="Form-input"
-            type="url"
-            value={movie_photo_url}
-            onChange={handleChange}/>
+    <div className="New-item-wrapper">
+      <form className="New-item-form-wrapper" onSubmit={handleSubmit}>
+        <h1>New Movie</h1>
+        <p className="Form-label">Title</p>
+        <input
+          className="Form-input"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <p className="Form-label">Release date</p>
+        <DatePicker
+          onChange={setReleaseDate}
+          value={releaseDate}
+        />
+        <p className="Form-label">Portait Photo</p>
+        <input
+          className="Form-input"
+          type="url"
+          value={moviePhotoUrl}
+          onChange={handleChange}/>
           &nbsp;
-          <img
-            src={photo_img_src}
-            alt={`${title} Poster`}
-            width="100"/>
-          {
-            !(movie_photo_url) &&
-            <p className="Form-label">Image will load with url</p> }
-          <div className="Submit-button-wrapper">
-            <input type="submit" value="Submit" className="Button Submit-button" />
-          </div>
-        </form>
-      </div>
+        <img
+          src={photoImgSrc}
+          alt={`${title} Poster`}
+          width="100"/>
+        {
+          !(moviePhotoUrl) &&
+            <p className="Form-label">Image will load with url</p>
+        }
+        <div className="Submit-button-wrapper">
+          <input
+            type="submit"
+            value="Submit"
+            className="Button Submit-button"/>
+        </div>
+      </form>
+    </div>
   );
 }
 
